0cb893424d6e2f192a2830f087be5d5714799ff5
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2023-01-23 16:00:14 +0530

    some nab ui, let's try rest stuff

--- app/src/main/java/com/example/steppy/MainActivity.kt
+++ app/src/main/java/com/example/steppy/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.steppy
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.steppy.ui.theme.SteppyTheme
+import kotlin.math.roundToInt
 
-class MainActivity : ComponentActivity() {
+class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +35,67 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SteppyApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SteppyApp() {
+    val context = LocalContext.current
+    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as? SensorManager
+    val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+    var steps by remember {
+        mutableStateOf(0)
+    }
+
+    if(stepSensor == null) {
+        Toast.makeText(context, "No sensor found", Toast.LENGTH_SHORT).show()
+    }
+    else {
+        sensorManager.registerListener(object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                steps = event!!.values[0].roundToInt()
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                TODO("Not yet implemented")
+            }
+
+        }, stepSensor, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Your steps")
+        ElevatedCard(
+            shape = CircleShape,
+            modifier = Modifier.size(250.dp)
+                .padding(20.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+                    .padding(10.dp)
+            ) {
+                Text(text = "$steps", modifier = Modifier.fillMaxSize(), fontSize = 150.sp, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SteppyTheme {
-        Greeting("Android")
+        SteppyApp()
     }
 }
